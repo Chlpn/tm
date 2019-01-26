0b25e7dc65737ba268f71094ba2323091501927b
Chlpn <prem@isi.ae>
2019-01-26 19:45:09 +0400

    update 2.6

--- Transaction Management/models/transaction_management.py
+++ Transaction Management/models/transaction_management.py
@@ -8,19 +8,19 @@ class TransMaster(models.Model):
     _description = "Transaction Management"
     _rec_name = 'transaction_no'
 
-    transaction_no = fields.Char(string='Transaction Number', readonly=True)
+    transaction_no = fields.Char(string='Transaction Number', readonly=True, store=True)
     transaction_date = fields.Date(string='Date',default=fields.Date.context_today, required=True)
     commission_included = fields.Boolean(string='is commission included')
     amount_to_swipe = fields.Float(string='Amount to Swipe')
     amount_to_customer = fields.Float(string='Amount to customer')
     commission = fields.Float(string='Commission')
-    cost_to_commission = fields.Float(string='Cost of Commission', readonly=True)
-    margin = fields.Float(string='margin', readonly=True)
+    cost_to_commission = fields.Float(string='Cost of Commission', readonly=True, store=True)
+    margin = fields.Float(string='margin', readonly=True, store=True)
     cash_paid_customer = fields.Float(string='Cash Paid')
-    balance = fields.Float(string='Balance', readonly=True)
+    balance = fields.Float(string='Balance', readonly=True, store=True)
     machine_name = fields.Many2one('machine.master')
     sales_percentage = fields.Float(string='Sales Percentage')
-    cost_percentage = fields.Float(string='Cost Percentage', readonly=True)
+    cost_percentage = fields.Float(string='Cost Percentage', readonly=True, store=True)
     customer = fields.Many2one('res.partner', string="Customer")
     state = fields.Selection([
         ('draft', 'Draft'),
@@ -37,7 +37,7 @@ class TransMaster(models.Model):
 
     @api.onchange('amount_to_customer','amount_to_swipe','sales_percentage')
     def _onchange_amount_to_customer(self):
-        if self.machine_name == '':
+        if not self.machine_name:
             raise UserError(_('Please select the Machine Name'))
         else:
 
