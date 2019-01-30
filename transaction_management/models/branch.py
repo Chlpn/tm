b26cf52b7a50863e45c7b7b61a6763fa50099cc4
Chlpn <prem@isi.ae>
2019-01-30 13:59:18 +0400

    update 5.2

--- transaction_management/models/branch.py
+++ transaction_management/models/branch.py
@@ -6,8 +6,11 @@ from odoo import fields, models
 class branch(models.Model):
     _inherit = ["multi.company.abstract"]
     _name = "company.branch"
+    _rec_name = "company_id"
 
-    name = fields.Char(string='Bank Name', required=True)
+    company_id =fields.Many2one('res.company', string="Branch", required=True, ondelete='restrict')
+    _sql_constraints = [('name_uniq', 'unique (company_id)', 'The branch name must be unique !')]
+
     cash_ac = fields.Many2one('account.account', string="Cash Account", required=True, ondelete='restrict',
                               domain=lambda self: [
                                   ('user_type_id', '=', self.env.ref('account.data_account_type_liquidity').id)])
