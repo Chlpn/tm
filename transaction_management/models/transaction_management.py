f33e5d22a1a7a9b55805ac79b6a4a41550e1bea3
Chlpn <prem@isi.ae>
2019-02-02 20:51:04 +0400

    update 11.0

--- transaction_management/models/transaction_management.py
+++ transaction_management/models/transaction_management.py
@@ -55,8 +55,8 @@ class TransMaster(models.Model):
         customer = self.customer.id
         self.env.cr.execute(
             """select sum(debit-credit) from account_move_line left join account_move on account_move_line.move_id=account_move.id where account_id=%s and account_move_line.partner_id=%s and  account_move.state='posted' group by account_id""",(account,customer))
-        bal = self.env.cr.fetchall()
-        return bal
+        self.customer_balance = self.env.cr.fetchall()
+        
 
 
 
