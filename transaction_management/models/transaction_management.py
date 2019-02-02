caf67296c7120a72b63c566e955194117e560428
Chlpn <prem@isi.ae>
2019-02-02 18:01:07 +0400

    update 9.1

--- transaction_management/models/transaction_management.py
+++ transaction_management/models/transaction_management.py
@@ -30,6 +30,10 @@ class TransMaster(models.Model):
     customer = fields.Many2one('res.partner', string="Customer", ondelete='restrict')
     customer_mobile = fields.Char(related='customer.mobile',string='Mobile')
     journal_ref = fields.Many2one('account.move', string="Accounting Reference")
+    customer_balance =fields.Float(string="Customer Balance")
+    machine_balance = fields.Float(string="Machine Balance")
+    cash_balance = fields.Float(string="Cash Balance")
+
     note = fields.Text(string="Notes")
 
     state = fields.Selection([
