d5f44d7502c6b09ae302f703ad4432377500c9df
Chlpn <prem@isi.ae>
2019-02-02 22:28:43 +0400

    update 11.2

--- transaction_management/models/transaction_management.py
+++ transaction_management/models/transaction_management.py
@@ -48,14 +48,13 @@ class TransMaster(models.Model):
         self.sales_percentage = self.machine_name.sales_percentage
         self.cost_percentage =self.machine_name.cost_percentage
 
-
     @api.onchange('customer')
     def _compute_cbal(self):
         account = self.customer.property_account_receivable_id.id
         customer = self.customer.id
         self.env.cr.execute(
             """select sum(debit-credit) from account_move_line left join account_move on account_move_line.move_id=account_move.id where account_id=%s and account_move_line.partner_id=%s and  account_move.state='posted' group by account_id""",(account,customer))
-        self.customer_balance = self.env.cr.fetchall()
+        self.customer_balance = self.env.cr.fetchall()[0]
 
 
 
