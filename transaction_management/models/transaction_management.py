4099396ba67c2f07601c1c3e2d3653bcc64713cd
Chlpn <prem@isi.ae>
2019-02-03 09:57:08 +0400

    update 12.1

--- transaction_management/models/transaction_management.py
+++ transaction_management/models/transaction_management.py
@@ -73,7 +73,7 @@ class TransMaster(models.Model):
             value = self.env.cr.fetchone()[0]
             self.env.cr.execute(
                 """select sum(debit-credit) from account_move_line left join account_move on account_move_line.move_id=account_move.id where account_id=%s and  account_move.state='posted' group by account_id""",
-                (caccount))
+                (caccount,))
             value2 = self.env.cr.fetchone()[0]
 
             if value is None:
