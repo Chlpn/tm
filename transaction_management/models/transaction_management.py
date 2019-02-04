6a4a66da536147c992e84f1d609c7208736bc0df
harilal <harilal@isi.ae>
2019-02-04 16:10:32 +0400

    name_search

--- transaction_management/models/transaction_management.py
+++ transaction_management/models/transaction_management.py
@@ -41,15 +41,6 @@ class TransMaster(models.Model):
         ('cancelled', 'Cancelled'),
     ], string='Status', default='draft',readonly=True)
 
-    @api.model
-    def name_search(self, name='', args=None, operator='ilike', limit=100):
-
-        res = super(TransMaster, self).name_search(name='', args=None, operator='ilike', limit=100)
-
-        ids = self.search(args + [('customer.mobile', '=', name)], limit=limit)
-
-        if ids:
-            return ids.name_get()
 
     @api.onchange('machine_name')
     def _onchange_machine_name(self):
@@ -324,4 +315,16 @@ class TransMaster(models.Model):
                 raise ValueError("Value of Amount to Customer must be greater than Zero")
             if record.sales_percentage <= 0:
                 raise ValueError("Value of Sales Percentage must be greater than Zero")
+
+
+class trans_ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        res = super(trans_ResPartner, self).name_search(name='', args=None, operator='ilike', limit=100)
+
+        ids = self.search(args + [('mobile', '=', name)], limit=limit)
+        return ids.name_get()
+
 
