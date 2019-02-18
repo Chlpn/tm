a5f0f026ad89a00ece7260a1921d04a3d6b5a6b3
Chlpn <prem@isi.ae>
2019-02-18 14:09:14 +0400

    update 25.4

--- transaction_management/models/credit_card_payment.py
+++ transaction_management/models/credit_card_payment.py
@@ -43,7 +43,7 @@ class ccPayment(models.Model):
         ('ps', 'Partially Swiped'),
         ('fs', 'Processed'),
         ('cl', 'Cancelled')
-    ], string='Status', default='dr',readonly=True)
+    ], string='Status' ,readonly=True)
 
 
     @api.onchange('payment_amount','commission')
