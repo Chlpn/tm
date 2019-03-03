6ecb24fd42b8854700546ad36c1e0e00618d94ca
harilal <harilal@isi.ae>
2019-03-03 14:47:30 +0400

    daily report

--- transaction_management/wizard/daily_report.py
+++ transaction_management/wizard/daily_report.py
@@ -6,4 +6,4 @@ class DailyReport(models.TransientModel):
     _name = "daily.report"
 
     report_date = fields.Date(string='Date', default=fields.Date.context_today, required=True)
-    report_branch = fields.Many2one('company.branch', string="branch", required=True)
+    report_branch = fields.Many2one('company.branch', string="branch")
