d7cf7c6f40a715ce4ebc25c86131edcb63f4856f
harilal <harilal@isi.ae>
2019-03-03 17:13:36 +0400

    daily report

--- transaction_management/wizard/daily_report.py
+++ transaction_management/wizard/daily_report.py
@@ -1,4 +1,4 @@
-from odoo import fields,models
+from odoo import fields,models,api
 from datetime import datetime
 
 
@@ -6,7 +6,9 @@ class DailyReport(models.TransientModel):
     _name = "daily.report"
 
     report_date = fields.Date(string='Date', default=fields.Date.context_today, required=True)
-    report_branch = fields.Many2one('company.branch', string="branch")
+    report_branch = fields.Many2one('company.branch', string="branch", required=True)
+    cash_op_bal =fields.Float()
+
 
 
 
@@ -16,3 +18,14 @@ class DailyReport(models.TransientModel):
             'type': 'ir.actions.report.xml',
             'report_name': 'transaction_management.daily_print_report'
         }
+
+    @api.multi
+    @api.onchange(report_date,report_branch)
+    def calc_values(self):
+
+        self.env.cr.execute("""select sum(debit-credit) from account_move_line where account_id=%s and date<%s""",
+                            (self.report_branch.cash_ac.id, self.report_date))
+        self.cash_op_bal = self.env.cr.fetchall()[0]
+        if self.cash_op_bal is None:
+            self.cash_op_bal = 0.0
+
