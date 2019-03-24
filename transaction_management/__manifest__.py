f8fdfd92daf8c2b16b10d0d5fe313ee11cf6f0d8
Chlpn <prem@isi.ae>
2019-03-24 15:13:15 +0400

    report 3

--- transaction_management/__manifest__.py
+++ transaction_management/__manifest__.py
@@ -21,8 +21,8 @@
                 'security/ir.model.access.csv',
                 'reports/report.xml',
                 'reports/invoice_report.xml',
-                'dynamic/custom_statement_customer.xml',
-                'dynamic/statement_customer.xml'
+                'dynamic/daily_report_view.xml',
+                'dynamic/daily_report.xml'
 
 
             ],
