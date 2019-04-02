b6e4d5557ea1a9e7b9db335de8c956bf95d05241
Chlpn <prem@isi.ae>
2019-04-02 13:39:56 +0400

    report 20

--- transaction_management/dynamic/render_daily_report.py
+++ transaction_management/dynamic/render_daily_report.py
@@ -25,7 +25,7 @@ class render_ldger(models.AbstractModel):
         ledger_data=report_obj.browse(docids)
 
 
-        self.env.cr.execute("""select (select name from res_partner where id=a.partner_id) as partner, sum(a.debit-a.credit) as balance from account_move_line as a left join account_move as b on a.move_id=b.id where a.account_id=19 and b.company_id=%s and b.state='posted' and a.date<=%s group by a.partner_id """,(ledger_data.company_id,datetime.datetime.strptime(ledger_data.report_date, '%Y-%m-%d'),))
+        self.env.cr.execute("""select (select name from res_partner where id=a.partner_id) as partner, sum(a.debit-a.credit) as balance from account_move_line as a left join account_move as b on a.move_id=b.id where a.account_id=19 and b.company_id=%s and b.state='posted' and a.date<=%s group by a.partner_id """,(ledger_data.branch_name,datetime.datetime.strptime(ledger_data.report_date, '%Y-%m-%d'),))
         datass = self.env.cr.fetchall()
 
         
