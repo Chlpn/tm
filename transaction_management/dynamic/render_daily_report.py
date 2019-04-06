ecaec4fe0d4f36555889ff2ff8b4e16b45d6640f
Chlpn <prem@isi.ae>
2019-04-06 20:17:07 +0400

    report 20

--- transaction_management/dynamic/render_daily_report.py
+++ transaction_management/dynamic/render_daily_report.py
@@ -26,7 +26,7 @@ class render_ldger(models.AbstractModel):
         ledger_data=report_obj.browse(docids)
 
         self.env.cr.execute(
-            """select * company_branch where company_id=%s""",(ledger_data.branch_name.id))
+            """select * company_branch where company_id=%s""",(ledger_data.branch_name.id,))
         vvalue = self.env.cr.fetchone()
         if vvalue is None:
             bid = 0
@@ -37,7 +37,9 @@ class render_ldger(models.AbstractModel):
         pay = vvalue.acc_payable_id
 
 
-        self.env.cr.execute("""select (select name from res_partner where id=a.partner_id) as partner, sum(a.debit-a.credit) as balance from account_move_line as a left join account_move as b on a.move_id=b.id where a.account_id=19 and b.company_id=%s and b.state='posted' and a.date<=%s group by a.partner_id """,(ledger_data.branch_name.id,datetime.datetime.strptime(ledger_data.report_date, '%Y-%m-%d'),))
+        self.env.cr.execute("""select (select name from res_partner where id=a.partner_id) as partner, sum(a.debit-a.credit) as balance from account_move_line as a left join
+         account_move as b on a.move_id=b.id where a.account_id=19 and b.company_id=%s and b.state='posted' and a.date<=%s group by 
+         a.partner_id """,(ledger_data.branch_name.id,datetime.datetime.strptime(ledger_data.report_date, '%Y-%m-%d'),))
         datass = self.env.cr.fetchall()
 
 
