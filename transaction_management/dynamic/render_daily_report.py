7d2c11c48acb14f26340d722492c83eba3f87b73
Chlpn <prem@isi.ae>
2019-04-10 10:26:01 +0400

    report 50

--- transaction_management/dynamic/render_daily_report.py
+++ transaction_management/dynamic/render_daily_report.py
@@ -52,8 +52,7 @@ class render_ldger(models.AbstractModel):
         else:
             ccb = cl[0]
 
-
-
+        # fetch customer balance
 
         self.env.cr.execute("""select (select name from res_partner where id=a.partner_id) as partner, sum(a.debit-a.credit) as balance from account_move_line as a left join
          account_move as b on a.move_id=b.id where a.account_id=%s and b.company_id=%s and b.state='posted' and a.date<=%s group by 
@@ -71,6 +70,8 @@ class render_ldger(models.AbstractModel):
                 move_dic={}
         
         data=move_list
+
+        # fetch vendor balance
 
         move_dic1 = {}
         move_list1 = []
@@ -91,6 +92,32 @@ class render_ldger(models.AbstractModel):
 
         data1 = move_list1
 
+        # fetch machine balance
+
+        move_dic2 = {}
+        move_list2 = []
+
+        self.env.cr.execute("""select (select name from machine_master where id=machine_name) as machine_name,sum(amount_to_swipe) 
+        as amount_to_swipe,sum(amount_to_customer) as amount_to_customer,sum(cash_paid_customer) as cash_paid_customer,sum(commission) 
+        as commission,sum(cost_to_commission) as cost_to_commission,sum(margin) as margin from trans_master where company_id=%s 
+        and transaction_date=%s and state='posted' group by machine_name""", (
+            cid,datetime.datetime.strptime(ledger_data.report_date, '%Y-%m-%d'),))
+        datass2 = self.env.cr.fetchall()
+
+        for moveline_data2 in datass2:
+            move_dic2['machine'] = moveline_data2[0]
+            move_dic2['amount_swiped'] = moveline_data2[1]
+            move_dic2['amount_pay'] = moveline_data2[2]
+            move_dic2['amount_paid'] = moveline_data2[3]
+            move_dic2['commission'] = moveline_data2[4]
+            move_dic2['cost_commission'] = moveline_data2[5]
+            move_dic2['margin'] = moveline_data2[6]
+
+            move_list2.append(move_dic2)
+            move_dic2 = {}
+
+        data2 = move_list2
+
 
         model = self.env.context.get('active_model')
         docs = self.env[model].browse(self.env.context.get('active_id'))
@@ -101,6 +128,7 @@ class render_ldger(models.AbstractModel):
             'ccb':ccb,
             'data': data,
             'data1': data1,
+            'data2': data2,
             'report_date':ledger_data.report_date,
             'branch_name':ledger_data.branch_name,
             'docs': docs,
