ab03e1d74647526f0068f847710164365558d825
Chlpn <prem@isi.ae>
2019-07-09 21:28:20 +0400

    .

--- transaction_management/dynamic/render_daily_report.py
+++ transaction_management/dynamic/render_daily_report.py
@@ -107,7 +107,7 @@ class render_ldger(models.AbstractModel):
                 """select sum(debit)-sum(credit) as expense from account_move_line as a left join account_account as b on a.account_id=b.id left join account_move as c on a.move_id = c.id where b.user_type_id = 16 and c.state='posted' and a.company_id=%s and a.date=%s""",
                 (cid,datetime.datetime.strptime(ledger_data.report_date, '%Y-%m-%d'),))
         gen = self.env.cr.fetchone()
-        if gen is None:
+        if type(gen[0]) is not 'Float':
             gex = 0
         else:
             gex = gen[0]
