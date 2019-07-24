3d7bc3cbb01dfdcbb6fdb0b2c3e6e1ad3a971ba6
Chlpn <prem@isi.ae>
2019-07-24 19:41:40 +0400

    .

--- transaction_management/models/all_wizard.py
+++ transaction_management/models/all_wizard.py
@@ -244,7 +244,7 @@ class SwipeCard(models.TransientModel):
 
             trans_master2 = self.env['trans.master'].create(vals)
 
-            mm_master.swipe(mm_master)
+            mm_master.swipe_card(mm_master)
 
 
 
