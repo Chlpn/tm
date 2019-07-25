46006734562d96eb7c917f5d8cabad3117adb7d1
Chlpn <prem@isi.ae>
2019-07-25 20:57:33 +0400

    .

--- transaction_management/models/transaction_management.py
+++ transaction_management/models/transaction_management.py
@@ -90,14 +90,14 @@ class TransMaster(models.Model):
                     else:
 
                         account = self.machine_name.merchant_bank_ac.id
-                    self.env.cr.execute(
+                        self.env.cr.execute(
                     """select sum(debit-credit) from account_move_line left join account_move on account_move_line.move_id=account_move.id where account_id=%s and  account_move.state='posted' group by account_id""",
                         (account,))
 
-                else:
-                    account = self.machine_name.rented_from.property_account_payable_id.id
-                    customer = self.machine_name.rented_from.id
-                    self.env.cr.execute(
+            else:
+                account = self.machine_name.rented_from.property_account_payable_id.id
+                customer = self.machine_name.rented_from.id
+                self.env.cr.execute(
                     """select sum(debit-credit) from account_move_line left join account_move on account_move_line.move_id=account_move.id where account_id=%s and account_move_line.partner_id=%s and  account_move.state='posted' group by account_id""",
                     (account, customer))
 
