90321770354cfeaeeeb9d552b00e34b1d81581f0
Chlpn <prem@isi.ae>
2019-08-28 13:25:31 +0400

    .

--- transaction_management/models/capital_drawing.py
+++ transaction_management/models/capital_drawing.py
@@ -12,17 +12,17 @@ class CapitalDrawing(models.Model):
 
     name = fields.Char(string='Name', size=64, readonly=True)
     calculation_date = fields.Date(string='Transaction Date', required=True, default=fields.Date.context_today)
-    previous_balance= fields.Float('Previous Balance' , readonly=True)
-    gross_profit = fields.Float('Gross Profit' , readonly=True)
-    expenses = fields.Float('Total Expenses', readonly=True)
-    net_profit = fields.Float('Net Profit', readonly=True)
-    lock_amount = fields.Float('received lock amount', readonly=True)
-    gross_amount = fields.Float('Amount to Pay', readonly=True)
-    amount_paid = fields.Float('Amount Paid', readonly=True)
-    amount_received = fields.Float('Amount Received', readonly=True)
-    net_amount = fields.Float('Net Balance', readonly=True)
-    payment_ref = fields.Many2one('account.move', string="Payment_ref", readonly=True)
-    receipt_ref = fields.Many2one('account.move', string="Receipt_ref", readonly=True)
+    previous_balance= fields.Float('Previous Balance' )
+    gross_profit = fields.Float('Gross Profit' )
+    expenses = fields.Float('Total Expenses')
+    net_profit = fields.Float('Net Profit')
+    lock_amount = fields.Float('received lock amount')
+    gross_amount = fields.Float('Amount to Pay')
+    amount_paid = fields.Float('Amount Paid')
+    amount_received = fields.Float('Amount Received')
+    net_amount = fields.Float('Net Balance')
+    payment_ref = fields.Many2one('account.move', string="Payment_ref")
+    receipt_ref = fields.Many2one('account.move', string="Receipt_ref")
     state = fields.Selection([
         ('dr', 'Draft'),
         ('cal', 'Calculated'),
